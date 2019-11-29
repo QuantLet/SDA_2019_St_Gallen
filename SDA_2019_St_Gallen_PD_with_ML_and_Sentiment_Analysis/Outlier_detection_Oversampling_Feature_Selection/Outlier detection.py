a892e1995c6565f98afb7867ebd8c1826af5baa5
jasminagk <46088730+jasminagk@users.noreply.github.com>
2019-11-29 12:28:06 +0100

    Update Outlier detection.py

--- SDA_2019_St_Gallen_PD_with_ML_and_Sentiment_Analysis/Outlier_detection_Oversampling_Feature_Selection/Outlier detection.py
+++ SDA_2019_St_Gallen_PD_with_ML_and_Sentiment_Analysis/Outlier_detection_Oversampling_Feature_Selection/Outlier detection.py
@@ -7,6 +7,7 @@ Created on Thu Nov 21 16:54:51 2019
 Input: X_all_train_wo_OS.csv, y_train_wo_OS.csv
 Output: y_all_inliers_wo_os.csv, x_all_inliers_wo_os.csv
 Purpose: Detect outliers using Isolation Forest (the percentage of outliers was set to 10%). Save new cleaned datasets with inliers.
+https://towardsdatascience.com/anomaly-detection-with-isolation-forest-visualization-23cd75c281e2
 """
 
 import os
